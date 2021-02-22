748684cda9b49bab48b112d58b4a80db1b5a7598
Pilat <maciekpilat@gmail.com>
2021-02-22 21:54:49 +0100

    angular added 4

--- src/main/java/pl/org/conceptweb/heydeskb/HeydeskbApplication.java
+++ src/main/java/pl/org/conceptweb/heydeskb/HeydeskbApplication.java
@@ -2,8 +2,9 @@ package pl.org.conceptweb.heydeskb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication( exclude = {SecurityAutoConfiguration.class} )
 public class HeydeskbApplication {
 
 	public static void main(String[] args) {
