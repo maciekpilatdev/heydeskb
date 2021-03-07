ed0fed3dbb22459bcbad07834ce001003110d7dd
Pilat <maciekpilat@gmail.com>
2021-03-07 21:57:21 +0100

    mvp + angular 7

--- src/main/java/pl/org/conceptweb/heydeskb/controller/MainController.java
+++ src/main/java/pl/org/conceptweb/heydeskb/controller/MainController.java
@@ -1,15 +1,15 @@
-package pl.org.conceptweb.heydeskb.controller;
-
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@CrossOrigin(origins = "https://heydeskb.herokuapp.com/", maxAge = 3600)
-public class MainController {
-
-    @GetMapping("/")
-    public String main() {
-        return "Welcome to uniappbackend!";
-    }
-}
+//package pl.org.conceptweb.heydeskb.controller;
+//
+//import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@CrossOrigin(origins = "https://heydeskb.herokuapp.com/", maxAge = 3600)
+//public class MainController {
+//
+////    @GetMapping("/")
+////    public String main() {
+////        return "Welcome to uniappbackend!";
+////    }
+//}
